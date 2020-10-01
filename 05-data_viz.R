b8c2a71c8540bb870735960be2a4b92aef499d31
picoral <adrianaps@email.arizona.edu>
2020-10-01 13:54:13 -0700

    updated script during class on oct 1

--- 05-data_viz.R
+++ 05-data_viz.R
@@ -41,10 +41,97 @@ spotify_data %>%
 spotify_data %>%
   ggplot(aes(x = release_year,
              y = track_popularity,
-             fill = playlist_genre)) +
-  geom_bin2d(alpha = 0.5)
+             color = playlist_genre)) +
+  geom_point(alpha = 0.5)
 
+########### October 01, 2020 ################
+# plot a scatterplot of track_populariy by release_year
+# add third variable to mapping in the plot, which is genre
+spotify_data %>%
+  ggplot(aes(x = release_year,
+             y = track_popularity,
+             color = playlist_genre)) +
+  geom_point(alpha = 0.3) +
+  facet_wrap(~playlist_genre)
 
+#### Summarize Data First ######
+spotify_summarised <- spotify_data %>%
+  group_by(release_year, playlist_genre, playlist_subgenre) %>%
+  summarise(mean_popularity = mean(track_popularity)) 
 
+# plot the summarized data, scatterplot of mean_popularity by
+# release year
+spotify_summarised %>%
+  ggplot(aes(x = release_year, y = mean_popularity)) +
+  geom_point()
 
+# plot the summarized data, scatterplot of mean_popularity (y)
+# by release year (x) across subgenre (color)
+spotify_summarised %>%
+  ggplot(aes(x = release_year, 
+             y = mean_popularity, 
+             color = playlist_subgenre)) +
+  geom_point()
+
+# plot the summarized data, bar plot of mean_popularity (y)
+# by release year (x) across subgenre (fill)
+spotify_summarised %>%
+  ggplot(aes(x = release_year,
+             y = mean_popularity,
+             fill = playlist_subgenre)) +
+  geom_col(position = "dodge")
+
+# same plot as before, but faceted
+spotify_summarised %>%
+  ggplot(aes(x = release_year,
+             y = mean_popularity,
+             fill = playlist_subgenre)) +
+  geom_col(position = "dodge") +
+  facet_wrap(~playlist_subgenre)
+
+# new summarized data frame
+# Summarize mean track_popularity by playlist_genre
+spotify_data %>%
+  group_by(playlist_genre) %>%
+  summarise(mean_popularity = mean(track_popularity)) %>%
+  ggplot(aes(x = fct_reorder(playlist_genre, -mean_popularity), 
+             y = mean_popularity)) +
+  geom_col() +
+  xlab("Genre") +
+  ylab("Mean Popularity") +
+  ggtitle("Mean Popularity of Tracks across Genre") +
+  theme_minimal()
+
+  
+# bar plot, stacked bars are subgenre, x is mapped to genre, and
+# y is mapped to mean_popularity
+spotify_data %>%
+  group_by(playlist_genre, playlist_subgenre) %>%
+  summarise(mean_popularity = mean(track_popularity)) %>%
+  ggplot(aes(x = playlist_genre,
+             y = mean_popularity,
+             fill = playlist_subgenre)) +
+  geom_col()
+
+# change to point and shape
+spotify_data %>%
+  group_by(playlist_genre, playlist_subgenre) %>%
+  summarise(mean_popularity = mean(track_popularity)) %>%
+  ggplot(aes(x = playlist_genre,
+             y = mean_popularity,
+             shape = playlist_subgenre)) +
+  geom_point()
+
+spotify_data %>% 
+  group_by(playlist_genre, playlist_subgenre) %>%
+  summarize(mean_popularity = mean(track_popularity)) %>% 
+  ggplot(aes(x = fct_reorder(playlist_genre, mean_popularity),
+             y = mean_popularity,
+             fill = playlist_subgenre)) +
+  geom_col() 
+
+spotify_data %>% 
+  group_by(playlist_genre, playlist_subgenre) %>%
+  summarize(mean_popularity = mean(track_popularity)) %>% 
+  arrange(-mean_popularity)
 
